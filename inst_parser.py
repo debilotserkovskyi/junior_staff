5964b54ef21387eaf07af461294c95664b05e4a2
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-05-03 19:27:05 +0300

    selenium is better for this

--- inst_parser.py
+++ inst_parser.py
@@ -1,19 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import re
 import requests
 from bs4 import BeautifulSoup
-
 from datetime import datetime
+import time
 
 link = 'https://www.instagram.com/accounts/login/'
 login_url = 'https://www.instagram.com/accounts/login/ajax/'
+url = 'https://www.instagram.com/'
 
-time = int(datetime.now().timestamp())
+
+fire_path = '/Users/maksimbelocerkovskij/dev_elo_pme_nt/geckodriver'
+driver = webdriver.Firefox(executable_path=fire_path)
+driver.get(url)
+
+
+time_ = int(datetime.now().timestamp())
 
 payload = {
     'username': 'test.cmon.nomc',
-    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time}:jCE9Ja3K8SY4n9Q',
+    'enc_password': f'#PWD_INSTAGRAM_BROWSER:0:{time_}:jCE9Ja3K8SY4n9Q',
+    'pass': 'jCE9Ja3K8SY4n9Q',
     'queryParams': {},
     'optIntoOneTap': 'false'
 }
@@ -30,9 +39,28 @@ with requests.Session() as s:
     print(r.status_code)
 
 
+hashtags = ["#cooking", "#ny"]
+
+response = requests.get(url).text
+bs = BeautifulSoup(response, 'html.parser')
+profile_name = []
+bio = []
+num_posts = []
+
+driver.\
+    find_element(by=By.XPATH,
+                 value='/html/body/div[1]/section/main/article/div[2]/div[1]/div[2]/form/div/div[1]/div/label/input')\
+    .send_keys(payload['username'], Keys.TAB, payload['pass'], Keys.ENTER)
+time.sleep(2)
 
 
+for i in hashtags:
+    driver.refresh()
+    driver.find_element(by=By.XPATH,
+                        value="/html/body/div[1]/section/nav/div[2]/div/div/div[2]/input").send_keys(i, Keys.ENTER)
 
+time.sleep(10)
+driver.quit()
 
 
 
