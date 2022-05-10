1f2acbd5becec06b9a6594d0889238f1324732e0
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-05-10 19:33:47 +0300

    add encode func

--- Pet projects/Message Encode Decode/main.py
+++ Pet projects/Message Encode Decode/main.py
@@ -6,8 +6,17 @@ window.geometry('500x300')
 window.title('Message Encode and Decode')
 
 
+def encode(key, message):
+    enc = []
+    
+    for i in range(len(message)):
+        key_c = key[i % len(key)]
+        enc.append(chr((ord(message[i] + ord(key_c))) % 256))
+    return base64.urlsafe_b64encode(''.join(enc).encode()).decode()
 
 
+def decode(key, message):
+    pass
 
 
 window.mainloop()
