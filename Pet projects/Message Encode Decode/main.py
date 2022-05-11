b22005c6c79ecda39087e8a1f95579b77bd740b8
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-05-11 21:15:58 +0300

    added frames

--- Pet projects/Message Encode Decode/main.py
+++ Pet projects/Message Encode Decode/main.py
@@ -1,10 +1,13 @@
 from tkinter import *
 import base64
 
-root = Tk()
-root.geometry('500x300')
-root.title('Message Encode and Decode')
-clip_text = root.clipboard_get()
+
+for frame in (f1, f2):
+    frame.grid(row=0, column=0, sticky='news')
+
+
+def raise_frame(frame):
+    frame.tkraise()
 
 
 def encode_b64():
@@ -25,12 +28,37 @@ def decode_b64():
     root.clipboard_append(str(base64.b64decode(message.encode(key)).decode(key)))
 
 
-Label(text='Print your message:').grid(columnspan=3, row=0)
-message_entry = Entry()
+# main
+root = Tk()
+root.geometry('500x300')
+root.title('Message Encode and Decode')
+
+# main frame
+f1 = Frame(root)
+# b64 frame
+f2 = Frame(root)
+# roman shift frame
+f3 = Frame(root)
+
+# start screen
+Label(f1, text='to decode or encode txt based on ascii').grid(columnspan=4, row=0)
+Button(f1, text='go', command=lambda: raise_frame(f2)).grid(columnspan=3, row=1)
+
+Label(f1, text='to decode/encode based on roman shift').grid(columnspan=5, row=2)
+Button(f1, text='go', command=lambda: raise_frame(f3)).grid(columnspan=3, row=3)
+
+# f2, based on ascii b45
+Label(f2, text='Print your message:').grid(columnspan=3, row=0)
+
+message_entry = Entry(f2)
 message_entry.grid(columnspan=3, row=1)
 
-Button(text='decode', command=decode_b64).grid(column=0, row=2)
-Button(text='encode', command=encode_b64).grid(column=2, row=2)
+Button(f2, text='decode', command=decode_b64).grid(column=0, row=2)
+Button(f2, text='encode', command=encode_b64).grid(column=2, row=2)
+
+# f3, based on shift (roman decode)
+Label(f3, text='type the shift number')
 
 
+raise_frame(f1)
 root.mainloop()
