8b32a8dd5934d6ad11b5005a5ad193fb533a3804
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-05-11 20:58:22 +0300

    changed window to root

--- Pet projects/Message Encode Decode/main.py
+++ Pet projects/Message Encode Decode/main.py
@@ -1,10 +1,10 @@
 from tkinter import *
 import base64
 
-window = Tk()
-window.geometry('500x300')
-window.title('Message Encode and Decode')
-clip_text = window.clipboard_get()
+root = Tk()
+root.geometry('500x300')
+root.title('Message Encode and Decode')
+clip_text = root.clipboard_get()
 
 
 def encode_b64():
@@ -12,8 +12,8 @@ def encode_b64():
     key = 'ascii'
     out_en = Label(text=base64.b64encode(message.encode(key)).decode(key))
     out_en.grid(columnspan=3, row=3)
-    window.clipboard_clear()
-    window.clipboard_append(str(base64.b64encode(message.encode(key)).decode(key)))
+    root.clipboard_clear()
+    root.clipboard_append(str(base64.b64encode(message.encode(key)).decode(key)))
 
 
 def decode_b64():
@@ -21,8 +21,8 @@ def decode_b64():
     key = 'ascii'
     out_de = Label(text=base64.b64decode(message.encode(key)).decode(key))
     out_de.grid(columnspan=3, row=5)
-    window.clipboard_clear()
-    window.clipboard_append(str(base64.b64decode(message.encode(key)).decode(key)))
+    root.clipboard_clear()
+    root.clipboard_append(str(base64.b64decode(message.encode(key)).decode(key)))
 
 
 Label(text='Print your message:').grid(columnspan=3, row=0)
@@ -32,4 +32,5 @@ message_entry.grid(columnspan=3, row=1)
 Button(text='decode', command=decode_b64).grid(column=0, row=2)
 Button(text='encode', command=encode_b64).grid(column=2, row=2)
 
-window.mainloop()
+
+root.mainloop()
