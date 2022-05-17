2f47efba5811bc51e5bcb45e1fb4ae71c2120722
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-05-17 22:58:00 +0300

    created playground

--- Pet projects/ Battleship Game/main.py
+++ Pet projects/ Battleship Game/main.py
@@ -1,6 +1,12 @@
-import sys, pygame
-from pygame.locals import*
-
+def play_field():
+    sea = [[chr(ord('A') + i)] for i in range(10)]
+    sea.append([str(i) + ' ' for i in range(0, 11)])
+    for row in range(len(sea)):
+        if row < 10:
+            sea[row].append(' 0 ' * 10)
+        print(' '.join(sea[row]))
+        
+    
 # TODO 1 Make multiple battleships:
 #  You will need to be careful because you need to make sure that you do not place battleships on top of each
 #  other on the game board.
@@ -13,4 +19,5 @@ from pygame.locals import*
 
 # TODO 3 Make your game a two-player game:
 #  Use functions to allow your game to have more features like rematches, statistics and more!
-    
+
+play_field()
