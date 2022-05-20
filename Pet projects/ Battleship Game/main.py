82000eb76f5c470b7079e6c063ce069f3768cc86
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-05-20 22:06:39 +0300

    can choose column and row to place a ship

--- Pet projects/ Battleship Game/main.py
+++ Pet projects/ Battleship Game/main.py
@@ -4,9 +4,9 @@ ships = {
     "Carrier": 5,
     "Battleship": 4,
     "Cruiser": 3,
-    "Submarine": 3,
-    "Destroyer": 2
-}
+    "Submarine": 2,
+    "Destroyer": 1
+}  # 1 2 3 4 5
 
 
 def sea():
@@ -22,25 +22,39 @@ def set_ships():
     global board
     row = 2
     # column = input('')
-    print(f'write where you want to place a ship in format (column or row) (letter or number) (ship name)\n{ships}')
-    place = input().title().split()
-    print(place)
-    column = int(place[2])
+    print(f'write where you want to place a ship in format (column or row) (letter or number) (number or letter) '
+          f'(ship name)\n{ships}')
     
+    place = input().title().split()
+    
+    print(place)
+    
+    # place ship for row
     if place[0] == 'R':
         if place[1] in 'ABCDEFGHIJ' and (place[2] in '123456789' or place[2] == '10'):
-            
+            column = int(place[2])
+    
             for i in board:
                 if place[1].upper() in i:
                     row = board.index(i)
-                    
+            
             for i in range(len(board[row][1])):
                 if place[3] in ships and i < ships[place[3]]:
                     board[row][1][column+i] = '1'
+    
+    # place ship for column
+    elif place[0] == 'Cl':
+        if (place[1] in '123456789' or place[1] == '10') and place[2] in 'ABCDEFGHIJ':
+            column = int(place[1])
+            
+            for i in board:
+                if place[2] in i:
+                    row = board.index(i)
 
-    elif place[0] == 'cl':
-        if place[1] in '123456789' or place[1] == '10':
-            pass
+            for i in range(len(board[row][1])):
+                if place[3] in ships and i < ships[place[3]]:
+                    board[row+i][1][column] = '1'
+        
     pprint(board)
     
     
