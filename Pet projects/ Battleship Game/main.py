7cc7b8c6997ace94eecbc06842110df40fb48a35
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-06-01 20:23:49 +0100

    :(

--- Pet projects/ Battleship Game/main.py
+++ Pet projects/ Battleship Game/main.py
@@ -1,10 +1,10 @@
 from pprint import pprint
 ships = {
-    "Carrier": 5,
-    "Battleship": 4,
-    "Cruiser": 3,
-    "Submarine": 2,
-    "Destroyer": 1
+    "Carrier": [5, 1],
+    "Battleship": [4, 2],
+    "Cruiser": [3, 3],
+    "Submarine": [2, 4],
+    "Destroyer": [1, 5]
 }  # 1 2 3 4 5
 global row
 
@@ -23,7 +23,34 @@ def sea():
     for i in range(10):
         i = [chr(ord('A') + i), ["O" for _ in range(10)]]
         board.append(i)
+
+    def add_ship():
+        global row
+        if place[0] == 'R':
+            if place[1] in 'ABCDEFGHIJ' and (place[2] in '123456789' or place[2] == '10'):
+                column = int(place[2])
+            
+                for i in board:
+                    if place[1].upper() in i:
+                        row = board.index(i)
+            
+                for i in range(len(board[row][1])):
+                    if place[3] in ships and i < ships[place[3]][0]:
+                        board[row][1][column + i] = '1'
     
+        # place ship for column
+        elif place[0] == 'Cl':
+            if (place[1] in '123456789' or place[1] == '10') and place[2] in 'ABCDEFGHIJ':
+                column = int(place[1])
+            
+                for i in board:
+                    if place[2] in i:
+                        row = board.index(i)
+            
+                for i in range(len(board[row][1])):
+                    if place[3] in ships and i < ships[place[3]][0]:
+                        board[row + i][1][column] = '1'
+                        
     while store_list <= 15:
         print(f'write where you want to place a ship in format (column or row) (letter or number) (number or letter)'
               f' (ship name)\n{ships}')
@@ -32,51 +59,18 @@ def sea():
         print(place)
         store_list += 1
         
-        if place[3] in store:
-            store[place[3]] += 1
-            if store['Destroyer'] > 5:
-                print('there could be only five')
-                store[place[3]] = 5
-            elif store['Submarine'] > 4:
-                print('there could be only four')
-                store[place[3]] = 4
-            elif store['Submarine'] > 3:
-                print('there could be only three')
-                store[place[3]] = 3
-            elif store['Submarine'] > 2:
-                print('there could be only two')
-                store[place[3]] = 2
-            elif store['Submarine'] > 1:
-                print('there could be only four')
-                store[place[3]] = 1
-                
         # place ship for row
-        if place[0] == 'R':
-            if place[1] in 'ABCDEFGHIJ' and (place[2] in '123456789' or place[2] == '10'):
-                column = int(place[2])
-                
-                for i in board:
-                    if place[1].upper() in i:
-                        row = board.index(i)
-                
-                for i in range(len(board[row][1])):
-                    if place[3] in ships and i < ships[place[3]]:
-                        board[row][1][column+i] = '1'
         
-        # place ship for column
-        elif place[0] == 'Cl':
-            if (place[1] in '123456789' or place[1] == '10') and place[2] in 'ABCDEFGHIJ':
-                column = int(place[1])
-                
-                for i in board:
-                    if place[2] in i:
-                        row = board.index(i)
-                
-                for i in range(len(board[row][1])):
-                    if place[3] in ships and i < ships[place[3]]:
-                        board[row+i][1][column] = '1'
+        store[place[3]] += 1
+        
+        for i in ships:
+            if store[i] > ships[i][1]:
+                break
+            else:
+                add_ship()
         
         pprint(board)
+        print(store)
         
             
 if __name__ == '__main__':
