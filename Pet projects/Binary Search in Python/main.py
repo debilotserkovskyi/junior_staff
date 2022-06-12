8adc931ce369e780ec6039578b078b1d2295df4c
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-06-12 19:42:08 +0100

    added some code

--- Pet projects/Binary Search in Python/main.py
+++ Pet projects/Binary Search in Python/main.py
@@ -1,3 +1,24 @@
-def binary_search():
-    pass
+def binary_search(arr, x, low, high):
+    
+    while low <= high:
+    
+        mid = (low + high) // 2
+        if arr[mid] < x:
+            low = mid + 1
+        elif arr[mid] > x:
+            low = mid - 1
+        else:
+            return mid
+        
+    return -1
 
+
+array_ = [2, 3, 5, 10, 40]
+x = 5
+result = binary_search(array_, x, 0, len(array_)-1)
+
+if result != -1:
+    print('element under index ' + str(result))
+else:
+    print('not found')
+    
