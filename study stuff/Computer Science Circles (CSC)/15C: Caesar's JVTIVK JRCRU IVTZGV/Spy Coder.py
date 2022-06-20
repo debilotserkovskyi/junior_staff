d480d630fc3bd71eb7467f3a389cbf6f33a29218
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-06-20 20:33:13 +0100

    ^^

--- study stuff/Computer Science Circles (CSC)/15C: Caesar's JVTIVK JRCRU IVTZGV/Spy Coder.py
+++ study stuff/Computer Science Circles (CSC)/15C: Caesar's JVTIVK JRCRU IVTZGV/Spy Coder.py
@@ -1,20 +1,37 @@
 def coder(txt: str, s:int):
     code_txt = ''
-    for i in txt:
-        if i == ' ':
-            code_txt += i
-        elif ord(i) == ord('Z'):
-            i = 'A'
-        elif ord('A') <= ord(i) <= ord('Z'):
-            if ord(i) + s >= ord('Z'):
-                k = ord(i) + 1 == ord('A')
-                code_txt += chr(ord(i)+s)
-            else:
-                code_txt += chr(ord(i)+s)
-        print(code_txt)
+    if s > 0:
+        for i in txt:
+            if i == ' ':
+                code_txt += i
+            elif ord('A') <= ord(i) <= ord('Z'):
+                if ord(i) + s >= ord('Z'):
+                    k, j = 0, 0
+                    while ord(i) + k < ord('Z'):
+                        k += 1
+                    j = s - k
+                    code_txt += chr(ord('A')+j-1)
+                else:
+                    code_txt += chr(ord(i)+s)
+    elif s < 0:
+        for i in txt:
+            if i == ' ':
+                code_txt += i
+            elif ord('A') <= ord(i) <= ord('Z'):
+                if ord(i) + s <= ord('A'):
+                    k, j = 0, 0
+                    while ord(i) - k > ord('A'):
+                        k += 1
+                    j = s + k
+                    code_txt += chr(ord('Z') + j + 1)
+                else:
+                    code_txt += chr(ord(i)+s)
+    else:
+        return txt
+    
     return code_txt
 
 
 if __name__ == '__main__':
-    print(coder('LQKP OG CV GKIJV DA VJG BQQ', -2))
+    print(coder('HUD', -6))
     
