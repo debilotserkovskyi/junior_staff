0cf4d68dde4b433062dfb191048014fa13a83f0c
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-06-23 19:56:12 +0100

    the last full delete

--- study stuff/Computer Science Circles (CSC)/15C: Caesar's JVTIVK JRCRU IVTZGV/Auto-Decryption.py
+++ study stuff/Computer Science Circles (CSC)/15C: Caesar's JVTIVK JRCRU IVTZGV/Auto-Decryption.py
@@ -21,7 +21,7 @@ words_list = []
 shifts = 0
 
 
-def decoder(txt: str, s: int):
+def decoder(txt: str, s: int = 1):
     """
     decode or encode a string depends on given str and shift
     :param txt: str
@@ -39,60 +39,8 @@ def decoder(txt: str, s: int):
     return new_txt
 
 
-def main(txt: str):
-    return words_list[value.index(txt_goodness([x for x in txt]))]
-    
-    
-def txt_goodness(txt_list: list):
-    global value, words_list, shifts, store
-    goodness_value = 0
-    position = 0
-    for char in txt_list:
-        if position == len(txt_list)-1:
-            goodness_value += store[char]
-            value.append(goodness_value)
-            nex_txt = ''.join(txt_list)
-            words_list.append(nex_txt)
-            shifts += 1
-            if shifts == 27:
-                comp_values()
-            else:
-                shift_by_one(nex_txt)
-        elif char == ' ':
-            position += 1
-            continue
-        else:
-            goodness_value += store[char]
-            position += 1
-    
-    
-def shift_by_one(new_txt: str):
-    shift = 1
-    while True:
-        position = 0
-        next_word = []
-        for char in new_txt:
-            if char == len(new_txt)-1:
-                next_word.append(decoder(char, shift))
-                return txt_goodness(next_word)
-            elif char == ' ':
-                next_word.append(' ')
-                position += 1
-            else:
-                next_word.append(decoder(char, shift))
-                position += 1
 
-
-def comp_values():
-    summarize = 0
-    position = 0
-    while position < len(value):
-        for val in value:
-            if val > summarize:
-                summarize = val
-                position += 1
-        return summarize
 
 if __name__ == '__main__':
-    main('LQKP OG')
+    decoder('LQKP')
     
