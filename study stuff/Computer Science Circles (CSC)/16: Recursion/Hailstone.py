a59f63561d88c6185e22671501788d9860d0b947
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-06-24 21:23:54 +0100

    done

--- study stuff/Computer Science Circles (CSC)/16: Recursion/Hailstone.py
+++ study stuff/Computer Science Circles (CSC)/16: Recursion/Hailstone.py
@@ -1,10 +1,11 @@
 def hailstone(n):
+    print(int(n))
     if n % 2 == 0:
         return hailstone(n/2)
     elif n == 1:
-        return n
+        return int(n)
     else:
         return hailstone(3*n+1)
   
 
-print(hailstone(5))
+print(hailstone(4))
