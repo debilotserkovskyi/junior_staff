9dbbb0434dc99dbc38d9114450548ca386157808
debilotserkovskyi <104721326+debilotserkovskyi@users.noreply.github.com>
2022-06-25 13:18:16 +0100

    T_T

--- study stuff/Computer Science Circles (CSC)/16: Recursion/Searching a Nested List.py
+++ study stuff/Computer Science Circles (CSC)/16: Recursion/Searching a Nested List.py
@@ -1,2 +1,11 @@
 def nestedListContains(NL, target):
-    pass
+    if isinstance(NL, int):
+        if NL == target:
+            return True
+    else:
+        counter = 0
+        while counter <= len(NL)-1:
+            if nestedListContains(NL[counter], target):
+                return True
+            counter += 1
+        return False
